6052e3ead8669908a643643bccb7029881dd3240
Samuel <samuelcolpaert@outlook.com>
2026-09-24 15:53:56 -0300

    Const All Themes

---
+++ script.js
@@ -0,0 +1,5 @@
+const body = document.body;
+const loginBox = document.querySelector('.login-box');
+const h1 = document.getElementsByTagName('h1')[0];
+const inputs = document.querySelectorAll('input');
+const loginButton = document.getElementById('login-button');
